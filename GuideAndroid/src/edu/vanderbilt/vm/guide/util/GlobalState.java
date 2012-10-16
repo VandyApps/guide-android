5a8f789dedb9a49944c44035c3d844f7fead3d33
Nick King <nicholas.b.king@vanderbilt.edu>
2012-10-15 19:59:29 -0500

    Map and Tours tabs added.  JSON parsing now works and is in action.  Adapters written for the agendas and lists of places.

--- GuideAndroid/src/edu/vanderbilt/vm/guide/util/GlobalState.java
+++ GuideAndroid/src/edu/vanderbilt/vm/guide/util/GlobalState.java
@@ -1,25 +1,38 @@
 package edu.vanderbilt.vm.guide.util;
 
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+
 /**
- * This class holds singletons of certain objects we need to share
- * throughout the application, such as the user's agenda.  This is
- * simpler and easier than using a SQLite database to hold the agenda
- * and allows us to use several methods to make data transactions
- * with the agenda easier.
+ * This class holds singletons of certain objects we need to share throughout
+ * the application, such as the user's agenda. This is simpler and easier than
+ * using a SQLite database to hold the agenda and allows us to use several
+ * methods to make data transactions with the agenda easier.
  * 
  * @author nicholasking
- *
+ * 
  */
 public class GlobalState {
 
 	private static Agenda userAgendaSingleton = new Agenda();
-	
-	private GlobalState() { 
+	private static List<Place> placeList;
+
+	private GlobalState() {
 		throw new AssertionError("Do not instantiate this class.");
 	}
-	
+
 	public static Agenda getUserAgenda() {
 		return userAgendaSingleton;
 	}
-	
+
+	public static List<Place> getPlaceList(Context context) throws IOException {
+		if (placeList == null) {
+			placeList = JsonUtils.readPlacesFromStream(context.getAssets()
+					.open("places.json"));
+		}
+		return placeList;
+	}
+
 }
---
+++ GuideAndroid/src/edu/vanderbilt/vm/guide/util/PlaceListAdapter.java
@@ -0,0 +1,51 @@
+package edu.vanderbilt.vm.guide.util;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class PlaceListAdapter extends BaseAdapter {
+
+	private List<Place> mPlaceList;
+	private Context mContext;
+
+	public PlaceListAdapter(Context context, List<Place> placeList) {
+		mContext = context;
+		mPlaceList = placeList;
+	}
+
+	@Override
+	public int getCount() {
+		return mPlaceList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mPlaceList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return mPlaceList.get(position).getUniqueId();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		TextView tv;
+		if (convertView == null) {
+			tv = (TextView) LayoutInflater.from(mContext).inflate(
+					android.R.layout.simple_list_item_1, null);
+			tv.setTag(tv);
+		} else {
+			tv = (TextView) convertView.getTag();
+		}
+		tv.setText(mPlaceList.get(position).getName());
+		return tv;
+	}
+
+}
