8ec909d3e54ca79b106e4333d3292aeec3a50c3a
Nick King <nicholas.b.king@vanderbilt.edu>
2012-11-23 02:28:13 -0600

    Progress commit on SQLite work

--- GuideAndroid/src/edu/vanderbilt/vm/guide/util/JsonUtils.java
+++ GuideAndroid/src/edu/vanderbilt/vm/guide/util/JsonUtils.java
@@ -6,61 +6,79 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import com.google.gson.stream.JsonReader;
 
+import edu.vanderbilt.vm.guide.db.GuideDBConstants;
+import edu.vanderbilt.vm.guide.db.GuideDBOpenHelper;
+import edu.vanderbilt.vm.guide.db.GuideDBConstants.PlaceTable;
+
 public class JsonUtils {
 
+	private static final int BAD_ID = -1;
+	private static final Logger logger = LoggerFactory
+			.getLogger("util.JsonUtils");
+
 	/**
-	 * Because this is just a class for static utility methods, this
-	 * class should not be instantiated.
+	 * Because this is just a class for static utility methods, this class
+	 * should not be instantiated.
 	 */
 	private JsonUtils() {
 		throw new AssertionError("Do not instantiate this class.");
 	}
-	
-	public static List<Place> readPlacesFromFile(Uri uri, Context context) throws IOException {
+
+	@Deprecated
+	public static List<Place> readPlacesFromFile(Uri uri, Context context)
+			throws IOException {
 		InputStream in = context.getContentResolver().openInputStream(uri);
 		return readPlacesFromStream(in);
 	}
-	
-	public static List<Place> readPlacesFromStream(InputStream in) throws IOException {
+
+	@Deprecated
+	public static List<Place> readPlacesFromStream(InputStream in)
+			throws IOException {
 		JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
 		List<Place> places = new ArrayList<Place>();
-		
+
 		reader.beginArray();
-		while(reader.hasNext()) {
+		while (reader.hasNext()) {
 			places.add(readPlace(reader));
 		}
 		return places;
 	}
-	
+
+	@Deprecated
 	public static Place readPlace(JsonReader reader) throws IOException {
 		Place.Builder bldr = new Place.Builder();
 		reader.beginObject();
-		while(reader.hasNext()) {
+		while (reader.hasNext()) {
 			String name = reader.nextName();
-			if(name.equals("id")) {
+			if (name.equals("id")) {
 				bldr.setUniqueId(reader.nextInt());
-			} else if(name.equals("name")) {
+			} else if (name.equals("name")) {
 				bldr.setName(reader.nextString());
-			} else if(name.equals("category")) {
+			} else if (name.equals("category")) {
 				bldr.setCategory(reader.nextString());
-			} else if(name.equals("hours")) {
+			} else if (name.equals("hours")) {
 				bldr.setHours(reader.nextString());
-			} else if(name.equals("placeDescription")) {
+			} else if (name.equals("placeDescription")) {
 				bldr.setDescription(reader.nextString());
-			} else if(name.equals("imagePath")) {
+			} else if (name.equals("imagePath")) {
 				bldr.setPictureUri(Uri.parse(reader.nextString()));
-			} else if(name.equals("videoPath")) {
+			} else if (name.equals("videoPath")) {
 				bldr.setVideoUri(Uri.parse(reader.nextString()));
-			} else if(name.equals("audioPath")) {
+			} else if (name.equals("audioPath")) {
 				bldr.setAudioUri(Uri.parse(reader.nextString()));
-			} else if(name.equals("latitude")) {
+			} else if (name.equals("latitude")) {
 				bldr.setLatitude(reader.nextDouble());
-			} else if(name.equals("longitude")) {
+			} else if (name.equals("longitude")) {
 				bldr.setLongitude(reader.nextDouble());
 			} else {
 				reader.skipValue();
@@ -69,5 +87,82 @@ public class JsonUtils {
 		reader.endObject();
 		return bldr.build();
 	}
-	
+
+	public static SQLiteDatabase populateDatabaseFromFile(String tableName,
+			Uri uri, Context context) throws IOException {
+		InputStream in = context.getContentResolver().openInputStream(uri);
+		return populateDatabaseFromInputStream(tableName, in, context);
+	}
+
+	public static SQLiteDatabase populateDatabaseFromInputStream(
+			String tableName, InputStream in, Context context)
+			throws IOException {
+		GuideDBOpenHelper helper = new GuideDBOpenHelper(context);
+		SQLiteDatabase db = helper.getWritableDatabase();
+		JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+
+		reader.beginArray();
+		if (tableName.equals(GuideDBConstants.PlaceTable.PLACE_TABLE_NAME)) {
+			while (reader.hasNext()) {
+				insertPlaceTuple(db, reader);
+			}
+		} else if (tableName.equals(GuideDBConstants.TourTable.TOUR_TABLE_NAME)) {
+			while (reader.hasNext()) {
+				insertTourTuple(db, reader);
+			}
+		}
+
+		return db;
+	}
+
+	private static void insertPlaceTuple(SQLiteDatabase db, JsonReader reader)
+			throws IOException {
+		reader.beginObject();
+
+		int id = BAD_ID;
+		ContentValues cv = new ContentValues();
+
+		while (reader.hasNext()) {
+			String propertyName = reader.nextName();
+			if (propertyName.equals("id")) {
+				id = reader.nextInt();
+			} else if (propertyName.equals("name")) {
+				cv.put(PlaceTable.NAME_COL, reader.nextString());
+			} else if (propertyName.equals("category")) {
+				cv.put(PlaceTable.CATEGORY_COL, reader.nextString());
+			} else if (propertyName.equals("hours")) {
+				cv.put(PlaceTable.HOURS_COL, reader.nextString());
+			} else if (propertyName.equals("placeDescription")) {
+				cv.put(PlaceTable.DESCRIPTION_COL, reader.nextString());
+			} else if (propertyName.equals("imagePath")) {
+				cv.put(PlaceTable.IMAGE_LOC_COL, reader.nextString());
+			} else if (propertyName.equals("videoPath")) {
+				cv.put(PlaceTable.VIDEO_LOC_COL, reader.nextString());
+			} else if (propertyName.equals("audioPath")) {
+				cv.put(PlaceTable.AUDIO_LOC_COL, reader.nextString());
+			} else if (propertyName.equals("latitude")) {
+				cv.put(PlaceTable.LATITUDE_COL, reader.nextDouble());
+			} else if (propertyName.equals("longitude")) {
+				cv.put(PlaceTable.LONGITUDE_COL, reader.nextDouble());
+			} else {
+				reader.skipValue();
+			}
+		}
+
+		reader.endObject();
+
+		if (id == BAD_ID) {
+			logger.warn("Got a place with no ID.  Skipping.");
+			return;
+		}
+
+		cv.put(PlaceTable.ID_COL, id);
+		db.insert(PlaceTable.PLACE_TABLE_NAME, null, cv);
+	}
+
+	private static void insertTourTuple(SQLiteDatabase db, JsonReader reader)
+			throws IOException {
+		// TODO: Implement.
+	}
+
 }
